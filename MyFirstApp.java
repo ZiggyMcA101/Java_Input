1b2738fedac013002ce9e4338a0f9d4f8cb2cb3f
Zakiya McAulay <65731903+ZiggyMcA101@users.noreply.github.com>
2021-01-25 23:47:34 -0500

    Add files via upload

---
+++ MyFirstApp.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstapp;
+
+/**
+ *
+ * @author Zemmy
+ */
+public class MyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("      A");
+        System.out.println("     AAA");
+        System.out.println("    AA AA");
+        System.out.println("   AA   AA ");
+        System.out.println("  AAAAAAAAA");
+        System.out.println(" AA       AA");
+        System.out.println("AA         AA");
+        // TODO code application logic here
+    }
+    
+}
